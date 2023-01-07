eb9e6b58d199fcc25a753b8ba9b7aa44e258ba74
Kristin Brewer <kr527106@gmail.com>
2023-01-07 18:53:30 -0500

    moved initial prompt questions back to index, trial test for manager

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const Manager = require("./lib/Manager");
+
+jest.mock('inquirer');
+
+describe(Manager, () => {
+    describe("managerInput", () => {
+        it("asks for a manager name", async () => {
+            
+        }
+        it("confirms input is a string"), () => {
+            const str = "";
+            expect(result).toBe("");
+        }
+    })
+});
--- index.js
+++ index.js
@@ -1,2 +1,56 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//calls scripts from library
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 
+
+
+//initial propmts to manager info
+function managerInput (managerInfo) {
+    return inquirer.prompt ([
+            {
+                name: 'name',
+                message: 'For managers: What is your name?',
+                type: 'input',
+            },
+            {
+                name: 'employeeId',
+                message: 'What is your employee ID?',
+                type: 'input',
+            },
+            {
+                name: 'email',
+                message: 'What is your employee email?',
+                type: 'input',
+            },
+            {
+                name: 'officeNum',
+                message: 'What is your office number?',
+                type: 'input',
+            },
+        ])
+        .then(managerInfo => {
+            let {
+                name,
+                employeeId,
+                email,
+                officeNum,
+            } = managerInfo;
+            console.log(managerInfo);
+        })
+    };
+
+
+        
+        //initializes app order
+function init() {
+    managerInput()
+    //.then
+ }
+ //calls function
+ init();
